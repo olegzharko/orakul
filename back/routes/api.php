5464665ea58c0a0fb5e10f111a6afad91c1afa67
ozharko <ozharko.unit@gmail.com>
2021-03-13 16:13:36 +0200

    passport access on

--- back/routes/api.php
+++ back/routes/api.php
@@ -12,6 +12,9 @@ use \App\Http\Controllers\Rakul\SlidesController;
 use \App\Http\Controllers\Rakul\SearchController;
 use \App\Http\Controllers\Rakul\SortController;
 use \App\Http\Controllers\Rakul\ManagerController;
+use \App\Http\Controllers\Factory\GeneratorController;
+use \App\Http\Controllers\Generator\DeveloperController;
+use \App\Http\Controllers\Generator\ImmovableController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,13 +42,34 @@ Route::post('password/forgot', [PassportAuthController::class, 'password_forgot'
 Route::get('password/reset/{token}', [PassportAuthController::class, 'password_reset'])->name('password.request');
 Route::post('password/update', [PassportAuthController::class, 'password_update'])->name('password.reset');
 
-Route::group(['prefix' => 'local'], function () {
-    Route::get('global_text', [TextController::class, 'global_text']);
-    Route::get('logout', [PassportAuthController::class, 'logout']);
+//Route::group(['prefix' => 'local'], function () {
+//    Route::get('global_text', [TextController::class, 'global_text']);
+//    Route::get('logout', [PassportAuthController::class, 'logout']);
+//    Route::get('extra_logout', [PassportAuthController::class, 'extra_logout']);
+//    Route::get('calendar', [CalendarController::class, 'calendar']);
+//    Route::put('cards/move/{id}', [CardController::class, 'move']);
+//    Route::resource('cards', CardController::class);
+//    Route::group(['prefix' => 'filter'], function () {
+//        Route::get('dropdown', [FilterController::class, 'dropdown']);
+//        Route::get('developer/info/{id}', [FilterController::class, 'developer_info']);
+//        Route::get('total', [ManagerController::class, 'total_cards']);
+//        Route::get('ready', [ManagerController::class, 'ready_cards']);
+//        Route::get('contract_type/{type}/{page}', [ManagerController::class, 'cards_by_contract_type']);
+//        Route::get('cancelled/{page}', [ManagerController::class, 'cancelled_cards']);
+//        Route::post('search', [SearchController::class, 'search']);
+//        Route::post('sort/{page}', [SortController::class, 'sort']);
+//    });
+//});
+
+Route::middleware('auth:api')->group(function () {
     Route::get('extra_logout', [PassportAuthController::class, 'extra_logout']);
+    Route::get('logout', [PassportAuthController::class, 'logout']);
+    Route::get('global_text', [TextController::class, 'global_text']);
     Route::get('calendar', [CalendarController::class, 'calendar']);
+    Route::resource('posts', PostController::class);
     Route::put('cards/move/{id}', [CardController::class, 'move']);
     Route::resource('cards', CardController::class);
+
     Route::group(['prefix' => 'filter'], function () {
         Route::get('dropdown', [FilterController::class, 'dropdown']);
         Route::get('developer/info/{id}', [FilterController::class, 'developer_info']);
@@ -56,24 +80,21 @@ Route::group(['prefix' => 'local'], function () {
         Route::post('search', [SearchController::class, 'search']);
         Route::post('sort/{page}', [SortController::class, 'sort']);
     });
-});
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('extra_logout', [PassportAuthController::class, 'extra_logout']);
-    Route::get('logout', [PassportAuthController::class, 'logout']);
-    Route::get('calendar', [CalendarController::class, 'calendar']);
-    Route::get('global_text', [TextController::class, 'global_text']);
-    Route::resource('posts', PostController::class);
-    Route::put('cards/move/{id}', [CardController::class, 'move']);
-    Route::resource('cards', CardController::class);
-    Route::group(['prefix' => 'filter'], function () {
-        Route::get('dropdown', [FilterController::class, 'dropdown']);
-        Route::get('developer/info/{id}', [FilterController::class, 'developer_info']);
-        Route::get('total', [ManagerController::class, 'total_cards']);
-        Route::get('ready', [ManagerController::class, 'ready_cards']);
-        Route::get('contract_type/{type}/{page}', [ManagerController::class, 'cards_by_contract_type']);
-        Route::get('cancelled/{page}', [ManagerController::class, 'cancelled_cards']);
-        Route::post('search', [SearchController::class, 'search']);
-        Route::post('sort/{page}', [SortController::class, 'sort']);
+    Route::group(['prefix' => 'generator'], function() {
+       Route::get('main/{card_id}', [\App\Http\Controllers\Generator\MainController::class, 'main']);
+       Route::get('create/{card_id}', [GeneratorController::class, 'creat_contract_by_card_id']);
+       Route::group(['prefix' => 'developer'], function() {
+           Route::get('main/{card_id}', [DeveloperController::class, 'main']);
+           Route::get('fence/{card_id}', [DeveloperController::class, 'get_fence']);
+           Route::post('fence/{card_id}', [DeveloperController::class, 'update_fence']);
+           Route::get('spouse/{card_id}', [DeveloperController::class, 'spouse']);
+           Route::get('representative/{card_id}', [DeveloperController::class, 'get_representative']);
+           Route::post('representative/{card_id}', [DeveloperController::class, 'update_representative']);
+       });
+
+        Route::group(['prefix' => 'immovable'], function() {
+            Route::get('main/{card_id}', [ImmovableController::class, 'main']);
+        });
     });
 });
