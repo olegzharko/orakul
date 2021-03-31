9727b6a23ec34d5156ae0fc406c2101f1e722c8d
ozharko <ozharki.unit@gmial.com>
2021-03-31 11:31:27 +0300

    fix sort

--- back/app/Http/Controllers/Rakul/SortController.php
+++ back/app/Http/Controllers/Rakul/SortController.php
@@ -28,35 +28,65 @@ class SortController extends BaseController
     public function sort(Request $r)
     {
         $result = null;
+        $cards_id = null;
         $validator = $this->validate_data($r);
 
         if (count($validator->errors()->getMessages())) {
             return $this->sendError('Форма передає помилкові дані', $validator->errors());
         }
 
-        $cards = Card::where(function ($query) use ($r) {
+        $query_cards_id = Card::select(
+            'cards.id',
+            'cards.notary_id',
+            'cards.room_id',
+            'cards.date_time',
+            'cards.city_id',
+            'cards.dev_company_id',
+            'cards.dev_representative_id',
+            'cards.dev_manager_id',
+            'cards.generator_step',
+            'cards.cancelled',
+        )->where(function ($query) use ($r) {
                 if ($r['notary_id'])
                     $query = $query->where('cards.notary_id', $r['notary_id']);
+                ######################### Старый вариант передачи данных с формы
+                if ($r['developer_id'])
+                    $query = $query->where('cards.dev_company_id', $r['developer_id']);
+                if ($r['giver_id'])
+                    $query = $query->where('contracts.accompanying_id', $r['giver_id']);
+                if ($r['dev_assistant_id'])
+                    $query = $query->where('cards.dev_representative_id', $r['dev_assistant_id']);
                 if ($r['dev_company_id'])
                     $query = $query->where('cards.dev_company_id', $r['dev_company_id']);
+                ########################## Конец старого варианта 
                 if ($r['dev_representative_id'])
                     $query = $query->where('cards.dev_representative_id', $r['dev_representative_id']);
                 if ($r['reader_id'])
                     $query = $query->where('contracts.reader_id', $r['reader_id']);
                 if ($r['accompanying_id'])
                     $query = $query->where('contracts.accompanying_id', $r['accompanying_id']);
+                if ($r['contract_type_id'])
+                    $query = $query->where('contracts.type_id', $r['contract_type_id']);
 
             return $query;
         })
             ->leftJoin('card_contract', 'cards.id', '=', 'card_contract.card_id')
             ->leftJoin('contracts', 'contracts.id', '=', 'card_contract.contract_id')
-            ->where('cards.date_time', '>=', $this->date)
-            ->get();
+            ->where('cards.date_time', '>=', $this->date->format('Y.m.d'))
+            ->distinct('cards.id');
+
+        if ($query_cards_id->pluck('cards.id'))
+            $cards_id = array_values(array_unique($query_cards_id->pluck('cards.id')->toArray()));
+
+        $cards_query = Card::whereIn('id', $cards_id)->whereIn('room_id', $this->rooms)->where('date_time', '>=', $this->date->format('Y.m.d'));
+
 
         if (auth()->user()->type == 'reception') {
-            $result = $this->card->get_cards_in_reception_format($cards, $this->rooms, $this->times, $this->date);
+            $cards = $cards_query->where('cancelled', false)->get();
+            $result = $this->card->get_cards_in_reception_format($cards);
         }
         elseif (auth()->user()->type == 'generator') {
+            $cards = $cards_query->where('staff_generator_id', auth()->user()->id)->where('generator_step', true)->get();
             $result = $this->card->get_cards_in_generator_format($cards);
         }
         else {
