5bd1348d7d068868b161c4cd7ac60d34dd2c88e4
Vadim Kisilov <49349216+Kisilov-Vadim@users.noreply.github.com>
2021-03-31 22:08:29 +0300

    add status circles to cards

--- front/src/components/Card/Card.tsx
+++ front/src/components/Card/Card.tsx
@@ -7,17 +7,37 @@ type Props = {
   children: React.ReactNode;
   link: string
   headerColor?: string;
+  haveStatus?: boolean;
 }
 
-const Card = ({ title, headerColor, children, link }: Props) => (
-  <Link to={link} className="card">
-    <div className="card__header" style={{ backgroundColor: headerColor || '' }}>
-      <span style={{ color: headerColor ? 'white' : '' }}>{title}</span>
-    </div>
-    <div className="card__main">
-      {children}
-    </div>
-  </Link>
-);
+const Card = ({ title, headerColor, children, link, haveStatus }: Props) => {
+  const getTextColor = () => {
+    if (haveStatus) return 'black';
+    if (headerColor) return 'white';
+
+    return '';
+  };
+
+  const getTitleBackgroundColor = () => {
+    if (haveStatus) return '';
+    if (headerColor) return headerColor;
+
+    return '';
+  };
+
+  return (
+    <Link to={link} className="card">
+      <div className="card__header" style={{ backgroundColor: getTitleBackgroundColor() }}>
+        <span style={{ color: getTextColor() }}>{title}</span>
+        {haveStatus && (
+          <div className="status" style={{ borderColor: headerColor || '' }} />
+        )}
+      </div>
+      <div className="card__main">
+        {children}
+      </div>
+    </Link>
+  );
+};
 
 export default Card;
