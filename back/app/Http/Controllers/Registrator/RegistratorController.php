83ecb45005cc7e113344d05f51a3084a68b9e737
ozharko <ozharki.unit@gmial.com>
2021-03-31 23:40:27 +0300

    check

--- back/app/Http/Controllers/Registrator/RegistratorController.php
+++ back/app/Http/Controllers/Registrator/RegistratorController.php
@@ -120,6 +120,7 @@ class RegistratorController extends BaseController
             return $this->sendError('Форма передає помилкові дані', $validator->errors());
         }
 
+
         DevFence::where('dev_company_id', $developer_id)->update([
             'date' => $r['date'],
             'number' => $r['number'],
