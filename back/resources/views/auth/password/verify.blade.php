7c3dd56e70639cf7fb9cf32715fed450c1eb77cb
ozharko <ozharko.unit@gmail.com>
2021-04-01 15:03:48 +0300

    add email to list

--- back/resources/views/auth/password/verify.blade.php
+++ back/resources/views/auth/password/verify.blade.php
@@ -2,13 +2,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Verify Your Email Address</div>
+                <div class="card-header">Відновлення пароля</div>
                 <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('Користувач '.$user_email.' відправив запит на відновлення пароля.') }}
-                        </div>
-                    @endif
+                    <div class="alert alert-success" role="alert">{{ __('Користувач '.$user_email.' відправив запит на відновлення пароля.') }}</div>
                     <a href="{{ url('api/password/reset/'.$token) }}">Click Here</a>.
                 </div>
             </div>
