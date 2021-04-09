301dd4f2a4e755640e4cd79c05dae1db1b8377e4
ozharko <ozharko.unit@gmail.com>
2021-04-09 20:58:57 +0300

    README.md back front

--- back/routes/api.php
+++ back/routes/api.php
@@ -168,7 +168,7 @@ Route::middleware('auth:api')->group(function () {
 });
 
 Route::get('clear_table', [\App\Http\Controllers\Test\ClearController::class, 'clear_table']);
-Route::get('dev_start_data', [\App\Http\Controllers\Test\DeveloperController::class, 'dev_start_data']);
+//Route::get('dev_start_data', [\App\Http\Controllers\Test\DeveloperController::class, 'dev_start_data']);
 Route::get('test', [\App\Http\Controllers\Test\ContractController::class, 'test']);
 Route::get('single_test', [\App\Http\Controllers\Test\ContractController::class, 'single_test']);
 
