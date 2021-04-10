a73a4eb230571a453fc8fd42e17630b8ad80a543
ozharko <ozharko.unit@gmail.com>
2021-04-10 18:27:04 +0300

    README.md back front

--- back/app/Http/Controllers/Generator/DeveloperController.php
+++ back/app/Http/Controllers/Generator/DeveloperController.php
@@ -165,8 +165,8 @@ class DeveloperController extends BaseController
     {
         $result = [];
 
-        dd($r['date']);
         $r['date'] = \DateTime::createFromFormat('d.m.Y H:i', $r['date']);
+        dd($r['date']);
 
         $validator = Validator::make([
             'date' => $r['date'],
