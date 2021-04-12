6ca57fd37ffd1af70b62a34243787bd9ac5497f6
ozharko <ozharko.unit@gmail.com>
2021-04-12 20:07:15 +0300

    add cards by manager

--- back/app/Http/Controllers/Rakul/CardController.php
+++ back/app/Http/Controllers/Rakul/CardController.php
@@ -64,11 +64,12 @@ class CardController extends BaseController
             $cards = $cards_query->where('cancelled', false)->get();
 
             $result = $this->get_cards_in_reception_format($cards);
-        }
-        elseif (auth()->user()->type == 'generator' || auth()->user()->type == 'manager') {
+        } elseif (auth()->user()->type == 'generator') {
             $cards = $cards_query->where('staff_generator_id', auth()->user()->id)
                 ->where('generator_step', true)->orderBy('date_time')->get();
-
+            $result = $this->get_cards_in_generator_format($cards);
+        } elseif (auth()->user()->type == 'manager') {
+            $cards = $cards_query->orderBy('date_time')->get();
             $result = $this->get_cards_in_generator_format($cards);
         }
         else {
