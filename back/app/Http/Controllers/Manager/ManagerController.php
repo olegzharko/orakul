cffe654b5a99894e20ee97864c18d0ec5206fb71
ozharko <ozharko.unit@gmail.com>
2021-04-15 19:40:51 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Http/Controllers/Manager/ManagerController.php
+++ back/app/Http/Controllers/Manager/ManagerController.php
@@ -343,10 +343,10 @@ class ManagerController extends BaseController
         $clients = Client::whereIn('id', $clients_id)->get();
 
         foreach ($clients as $key => $client) {
-            $result[$key]['client'] = [];
-            $result[$key]['client']['id'] = $client->id;
-            $result[$key]['client']['full_name'] = $this->convert->get_full_name($client);
-            $result[$key]['client']['list'] = ['Teст 1', 'Тест 2', 'Test 3'];
+            $result[$key] = [];
+            $result[$key]['id'] = $client->id;
+            $result[$key]['full_name'] = $this->convert->get_full_name($client);
+            $result[$key]['list'] = ['Teст 1', 'Тест 2', 'Test 3'];
         }
 
         return $this->sendResponse($result, 'Клієнти по карточці ID' . $card_id);
