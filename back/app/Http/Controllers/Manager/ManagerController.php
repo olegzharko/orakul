1b74b32d29e350cb678c9039ad05b21c17bbf9c7
ozharko <ozharko.unit@gmail.com>
2021-04-15 22:31:15 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Http/Controllers/Manager/ManagerController.php
+++ back/app/Http/Controllers/Manager/ManagerController.php
@@ -444,19 +444,19 @@ class ManagerController extends BaseController
         }
 
         if ($r['client'] && count($r['client']['data'])) {
-            $client_id = $this->create_or_update_client($card_id, $client_id, $r['client']);
+            $client_id = $this->create_or_update_client($card_id, $client_id, $r['client']['data']);
             if ($client_id)
                 $this->card_client($client_id, $card_id);
         }
 
         if ($r['spouse'] && count($r['client']['data'])) {
-            $spouse_id = $this->create_or_update_client($card_id, $client_id, $r['spouse']);
+            $spouse_id = $this->create_or_update_client($card_id, $client_id, $r['spouse']['data']);
             if ($spouse_id)
                 $this->client_spouse($client_id, $spouse_id);
         }
 
         if ($r['confidant'] && count($r['client']['data'])) {
-            $representative_id = $this->create_or_update_client($card_id, $client_id, $r['confidant']);
+            $representative_id = $this->create_or_update_client($card_id, $client_id, $r['confidant']['data']);
             if ($representative_id)
                 $this->client_representative($client_id, $representative_id);
         }
