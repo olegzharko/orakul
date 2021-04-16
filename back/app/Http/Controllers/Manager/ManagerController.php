db1f779ff120a5ab7567d15002fb8399cf31a1ab
ozharko <ozharko.unit@gmail.com>
2021-04-16 08:09:35 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Http/Controllers/Manager/ManagerController.php
+++ back/app/Http/Controllers/Manager/ManagerController.php
@@ -431,7 +431,7 @@ class ManagerController extends BaseController
     {
         $new = false;
 
-        dd($r->toArray());
+        dd($r->toArray(), $r['client'],$r['client']['data'], count($r['client']['data']));
 
         if ($client_id && !$client = Client::find($client_id)) {
             return $this->sendError('', 'Клієнт під ID:' . $client_id . ' відсутній.');
