ec559f3ac59d1285deeb23d6349d6bc3707e7582
ozharko <ozharko.unit@gmail.com>
2021-04-16 17:46:19 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Http/Controllers/Generator/ClientController.php
+++ back/app/Http/Controllers/Generator/ClientController.php
@@ -514,8 +514,8 @@ class ClientController extends BaseController
 
         if (count($contracts_id)) {
             foreach ($contracts_id as $contr_id) {
-                dd($contr_id, $client_spouse_consent_id);
-                ClientSpouseConsentContract::updateOrCreate(['contract_id' => $contr_id], ['client_spouse_consent_id' => $client_spouse_consent_id]);
+                if ($contr_id)
+                    ClientSpouseConsentContract::updateOrCreate(['contract_id' => $contr_id], ['client_spouse_consent_id' => $client_spouse_consent_id]);
             }
         }
 
