d98379ecb809247984007759e187f745e1d1d892
ozharko <ozharko.unit@gmail.com>
2021-04-21 13:45:55 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Http/Controllers/Factory/DocumentController.php
+++ back/app/Http/Controllers/Factory/DocumentController.php
@@ -1495,7 +1495,7 @@ class DocumentController extends GeneratorController
             $word->setValue('pr-reg-num', $this->contract->immovable->proxy->reg_num);
 
             $word->setValue('ДД-НОМЕР', $this->contract->immovable->proxy->number);
-            $word->setValue('ДД-ДАТА', $this->day_quotes_month_year($this->contract->immovable->proxy->date));
+            $word->setValue('ДД-ДАТА', $this->display_date($this->contract->immovable->proxy->date));
             $word->setValue('ДД-НОТ-ПІБ-ІНІЦІАЛИ', $this->convert->get_surname_and_initials($this->contract->immovable->proxy->notary));
             $word->setValue('ДД-НОТ-ДАТА', $this->display_date($this->contract->immovable->proxy->reg_date));
             $word->setValue('ДД-НОТ-НОМЕР', $this->contract->immovable->proxy->reg_num);
