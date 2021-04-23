f3e1dc642df708554cadcd0831b0eead9475e86b
Vadim Kisilov <49349216+Kisilov-Vadim@users.noreply.github.com>
2021-04-23 18:53:43 +0300

    add notary to generator/immovable/ownership

--- front/src/Screens/DashboardScreen/components/ManageContainer/components/WorkSpace/components/Immovable/components/GeneratorContainer/components/ImmovableFields/components/Ownership/useOwnership.ts
+++ front/src/Screens/DashboardScreen/components/ManageContainer/components/WorkSpace/components/Immovable/components/GeneratorContainer/components/ImmovableFields/components/Ownership/useOwnership.ts
@@ -10,6 +10,8 @@ type InitialData = {
   reg_number: number | string;
   discharge_date: any;
   discharge_number: number | string;
+  notary_id: string | null;
+  notary?: any,
 }
 
 export type Props = {
@@ -21,12 +23,15 @@ export const useOwnership = ({ initialData, id }: Props) => {
   const dispatch = useDispatch();
   const { token } = useSelector((state: State) => state.main.user);
 
+  const [notary, setNotary] = useState([]);
+
   // Initial data
   const [data, setData] = useState<InitialData>({
     reg_date: null,
     reg_number: '',
     discharge_date: null,
     discharge_number: '',
+    notary_id: null,
   });
 
   const onClear = useCallback(() => {
@@ -35,6 +40,7 @@ export const useOwnership = ({ initialData, id }: Props) => {
       reg_number: '',
       discharge_date: null,
       discharge_number: '',
+      notary_id: null,
     });
   }, []);
 
@@ -64,11 +70,14 @@ export const useOwnership = ({ initialData, id }: Props) => {
       discharge_date: initialData?.discharge_date
         ? new Date(changeMonthWitDate(initialData?.discharge_date)) : null,
       discharge_number: initialData?.discharge_number || '',
+      notary_id: initialData?.notary_id || null,
     });
+    setNotary(initialData?.notary || []);
   }, [initialData]);
 
   return {
     data,
+    notary,
     setData,
     onClear,
     onSave,
