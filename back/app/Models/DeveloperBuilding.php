e8a8b12acecb928b747e0d5fa032ab43124cf021
ozharko <ozharko.unit@gmail.com>
2021-05-11 13:48:36 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Models/DeveloperBuilding.php
+++ back/app/Models/DeveloperBuilding.php
@@ -56,7 +56,7 @@ class DeveloperBuilding extends Model implements Sortable
 
     public static function get_dev_group_buildings($buildings_id)
     {
-        return DeveloperBuilding::whereIn('id', $buildings_id)->orderBy('developer_buildings.title')->get();
+        return DeveloperBuilding::whereIn('id', $buildings_id)->orderBy('address_type_id')->orderBy('title')->orderBy('number')->get();
     }
 
     public static function get_developer_building($dev_company_id)
