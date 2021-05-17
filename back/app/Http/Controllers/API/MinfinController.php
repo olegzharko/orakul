58f4f464374912c42b190bf59bb61636cb68e0c7
ozharko <ozharko.unit@gmail.com>
2021-05-17 16:47:34 +0300

    README.md app artisan bootstrap composer.json composer.lock config database nova package.json phpunit.xml public resources routes server.php storage tests vendor webpack.mix.js

--- back/app/Http/Controllers/API/MinfinController.php
+++ back/app/Http/Controllers/API/MinfinController.php
@@ -10,30 +10,53 @@ use GuzzleHttp\Client;
 
 class MinfinController extends BaseController
 {
+    public $client;
+
     public function __construct()
     {
-
+        $this->client = new \GuzzleHttp\Client();
     }
 
     public function get_rate_exchange()
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://api.minfin.com.ua/auction/info/7a176e1592eb3b008f05ccd42c78f9d2c81e461c/');
-//        $response = $client->request('GET', 'https://api.minfin.com.ua/auction/info/7a176e1592eb3b008f05ccd42c78f9d2c81e461c/');
-
-        $api_data = json_decode($response->getBody());
-
-        if (!$api_data->usd)
-            return $this->sendError('', 'Запит не передав дані');
-        $ask = $api_data->usd->ask;
-        $currency_exchage = round($ask, 2);
+        $currency_exchage = $this->get_dollar_auction();
+        $contract = $this->get_contract_buy();
 
         $exchange = new Exchange();
         $exchange->rate = $currency_exchage * 100;
+        $exchange->contract_buy = $contract ? round($contract->buy, 2) * 100 : null;
+        $exchange->contract_sell = $contract ? round($contract->sell, 2) * 100 : null;
         $exchange->save();
 
         $result['exchange_rate'] = $currency_exchage;
 
         return $this->sendResponse($result, 'Новий курс додано до базы даних');
+    }
+
+    public function get_contract_buy()
+    {
+        $response = $this->client->request('GET', 'https://api.minfin.com.ua/contracts/7a176e1592eb3b008f05ccd42c78f9d2c81e461c/');
+
+        $api_data = json_decode($response->getBody());
+
+        if (!$api_data->data || !$api_data->data->auction)
+            return $this->sendError('', 'Запит не передав дані');
+
+        return $api_data->data->auction;
+    }
+
+    public function get_dollar_auction()
+    {
+        $response = $this->client->request('GET', 'https://api.minfin.com.ua/auction/info/7a176e1592eb3b008f05ccd42c78f9d2c81e461c/');
+
+        $api_data = json_decode($response->getBody());
+
+        if (!$api_data->usd)
+            return $this->sendError('', 'Запит не передав дані');
+
+        $ask = $api_data->usd->ask;
+        $currency_exchage = round($ask, 2);
+
+        return $currency_exchage;
     }
 }
