13032601c77416cb239b85be099eb4b267e1dfb4
ozharko <ozharki.unit@gmial.com>
2021-07-26 10:15:07 +0300

    udpate storage

--- back/storage/framework/views/344291d74025d565c2e253f221a2a73713ea0a6f.php
+++
@@ -1,2 +0,0 @@
-
-<?php /**PATH /home/ozharko/synction.space/app/orakul/back/resources/views/vendor/nova/partials/meta.blade.php ENDPATH**/ ?>
--- back/storage_old/framework/views/8c74390a612ca4baeebffe5eb7444e0f55e8257a.php
+++
@@ -1,2 +0,0 @@
-
-<?php /**PATH /var/www/orakul/back/resources/views/vendor/nova/partials/meta.blade.php ENDPATH**/ ?>
