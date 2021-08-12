e254eaa6ba669d623bd55c06f373f58c2979c980
ozharko <ozharki.unit@gmial.com>
2021-08-12 09:57:52 +0300

    add global user reception type

--- back/storage_del_me/framework/views/ae4e70ec74066efd13650f46c8813cf513107015.php
+++
@@ -1,2 +0,0 @@
-<?php echo $__env->make('nova::partials.logo', ['width' => '200', 'height' => '39'], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php /**PATH /home/ozharko/synction.space/app/orakul/back/nova/src/../resources/views/auth/partials/logo.blade.php ENDPATH**/ ?>
