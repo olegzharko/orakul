a347934b266ce690f5c89ac8231a7b052860336d
olegzharko <olegzharko@gmail.com>
2024-08-30 11:55:34 +0300

    add notarize for reducer

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -4,18 +4,20 @@ import main from './main/store';
 import scheduler from './scheduler/store';
 import appointments from './appointments/store';
 import filter from './filter/store';
-import registrator from './registrator/store';
 import clients from './clients/store';
 import immovables from './immovables/store';
+import registrator from './registrator/store';
+import notarize from './notarize/store';
 
 const reducer = combineReducers({
   main,
   scheduler,
   appointments,
   filter,
-  registrator,
   clients,
   immovables,
+  registrator,
+  notarize,
 });
 
 export default createStore(reducer, applyMiddleware(thunk));
